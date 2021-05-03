4941b85f61f88c45755fbee72109ad499956978b
Qiyun Dai <pianoquin@gmail.com>
2021-05-03 11:48:00 -0500

    I don't have either but here's a LG for them.

---
+++ split-LP/script.js
@@ -0,0 +1,9 @@
+const $left = document.querySelector('.left');
+const $right = document.querySelector('.right');
+const $containter = document.querySelector('.container');
+
+$left.addEventListener('mouseenter', () => $containter.classList.add('hover-left'));
+$left.addEventListener('mouseleave', () => $containter.classList.remove('hover-left'));
+
+$right.addEventListener('mouseenter', () => $containter.classList.add('hover-right'));
+$right.addEventListener('mouseleave', () => $containter.classList.remove('hover-right'));
